b23f5d67c3ea743db52f4fafc424dcc047242d8e
imhamza1 <hamza.akram4635@gmail.com>
2020-12-03 13:12:03 +0500

    nav updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var bodyParse = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
@@ -19,8 +20,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
